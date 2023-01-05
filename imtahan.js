2d7563db582f3d038f8eeb96cb0783ac9bf895ad
MehemmedQuliyev <115102076+MehemmedQuliyev@users.noreply.github.com>
2023-01-05 22:05:31 +0400

    Add files via upload

---
+++ imtahan.js
@@ -0,0 +1,17 @@
+function openNav(){
+    document.getElementById('sidebar').style.left="0px";
+    document.getElementById('sback').style.zIndex="49";
+}
+function closeNav(){
+    document.getElementById('sidebar').style.left="-250px";
+    document.getElementById('sback').style.zIndex="-1"
+
+    
+}
+
+
+/*
+function togglesidebar(){
+    document.getElementById('sidebar').classList.toggle('active');
+}
+*/
